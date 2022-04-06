0ebf02ea29768f60c3024a60379ef211743363d3
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-06 15:53:58 +0200

    Created car valuation component

---
+++ src/components/CarValuation/CarValuation.tsx
@@ -0,0 +1,28 @@
+import { ValuationI } from '../../interfaces/cars.interfaces';
+import './CarValuation.scss';
+
+function CarValuation({
+    valuationOverTime,
+}: {
+    valuationOverTime: ValuationI[];
+}) {
+    return (
+        <div className="valuation">
+            <div className="valuation__title-container">
+                <p className="valuation__title">Valoración Venal</p>
+            </div>
+            {valuationOverTime.map((item) => (
+                <div key={item.year} className="valuation__row">
+                    <p className="valuation__cell valuation__cell--field">
+                        {item.year}
+                    </p>
+                    <p className="valuation__cell">
+                        {item.value.toLocaleString()}
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default CarValuation;
