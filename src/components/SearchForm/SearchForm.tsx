72397b6184a5f916c147cdc4bfd9e9f46e634c60
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-06 11:17:57 +0200

    Added form controls and apiRequest for models

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { BRANDS, FUEL } from '../../data/car-data';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { BRANDS, FUEL } from '../../data/carData';
+import { CarI } from '../../interfaces/cars.interfaces';
+import { getCars } from '../../services/apiRequest';
 
 function SearchForm() {
+    const [formValue, setFormValue] = useState({
+        brand: '',
+        enrollmentDate: '',
+        fuel: '',
+    });
+
+    const [cars, setCars] = useState<CarI[]>([]);
+
+    const handleChange = (e: ChangeEvent) => {
+        const target = e.target as HTMLSelectElement;
+        setFormValue({ ...formValue, [target.name]: target.value });
+    };
+
+    useEffect(() => {
+        if (
+            formValue.brand !== '' &&
+            formValue.enrollmentDate !== '' &&
+            formValue.fuel !== ''
+        ) {
+            getCars(
+                formValue.brand,
+                formValue.enrollmentDate,
+                formValue.fuel
+            ).then((resp) => {
+                console.log(resp.data);
+                setCars(resp.data.cars);
+            });
+        }
+    }, [formValue]);
+
     return (
         <form>
             <div>
                 <label htmlFor="brand">Marca</label>
-                <select name="brand" id="brand">
+                <select
+                    name="brand"
+                    id="brand"
+                    value={formValue.brand}
+                    onChange={handleChange}
+                >
+                    <option disabled selected>
+                        {' '}
+                        -- select an option --{' '}
+                    </option>
                     {BRANDS.map((brand) => (
                         <option value={brand}>{brand}</option>
                     ))}
@@ -17,16 +58,44 @@ function SearchForm() {
                     Fecha de la Primera Matriculación
                 </label>
                 <input
+                    value={formValue.enrollmentDate}
                     type="date"
-                    name="initial-enrollment-date"
+                    name="enrollmentDate"
                     id="initial-enrollment-date"
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor="fuel">Combustible</label>
-                <select name="fuel" id="fuel">
+                <select
+                    name="fuel"
+                    id="fuel"
+                    value={formValue.fuel}
+                    onChange={handleChange}
+                >
+                    <option disabled selected>
+                        {' '}
+                        -- select an option --{' '}
+                    </option>
                     {FUEL.map((fuel) => (
                         <option value={fuel}>{fuel}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="model">Modelo</label>
+                <select
+                    name="model"
+                    id="model"
+                    // value={formValue.fuel}
+                    // onChange={handleChange}
+                >
+                    <option disabled selected>
+                        {' '}
+                        -- select an option --{' '}
+                    </option>
+                    {cars.map((car) => (
+                        <option value={car.model}>{car.model}</option>
                     ))}
                 </select>
             </div>
