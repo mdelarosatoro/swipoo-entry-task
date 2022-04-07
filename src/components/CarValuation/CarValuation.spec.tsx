29c9f26ec7311046798ad4814c5b263c737856e1
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-07 10:46:40 +0200

    Added CarValuation component test

---
+++ src/components/CarValuation/CarValuation.spec.tsx
@@ -0,0 +1,30 @@
+import { prettyDOM, render, screen, waitFor } from '@testing-library/react';
+import CarValuation from './CarValuation';
+
+describe('Given the CarValuation component', () => {
+    describe('When called to render with an array of valuations', () => {
+        test('It should render the valuations on the document', () => {
+            const arrayOfValuations = [
+                { year: 2020, value: 30000 },
+                { year: 2021, value: 27000 },
+                { year: 2022, value: 24300 },
+            ];
+            const { container } = render(
+                <CarValuation valuationOverTime={arrayOfValuations} />
+            );
+
+            console.log(prettyDOM(container));
+            waitFor(() => {
+                expect(
+                    screen.getByText(/Valoración Venal/)
+                ).toBeInTheDocument();
+                expect(arrayOfValuations[0].year).toBeInTheDocument();
+                expect(arrayOfValuations[0].value).toBeInTheDocument();
+                expect(arrayOfValuations[1].year).toBeInTheDocument();
+                expect(arrayOfValuations[1].value).toBeInTheDocument();
+                expect(arrayOfValuations[2].year).toBeInTheDocument();
+                expect(arrayOfValuations[2].value).toBeInTheDocument();
+            });
+        });
+    });
+});
