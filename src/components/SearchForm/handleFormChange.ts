18c507502efa99664f05a9448aa8bfbdf6c123e8
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-11 12:41:58 +0200

    Abstracted formChange handler to anotnher file

---
+++ src/components/SearchForm/handleFormChange.ts
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction } from 'react';
+import { FormValueI } from '../../interfaces/form.interfaces';
+
+export const handleFormChange = (
+    setFormValue: Dispatch<SetStateAction<FormValueI>>,
+    formValue: FormValueI,
+    targetName: string,
+    targetValue: string
+) => {
+    let newState;
+    if (targetName === 'brand') {
+        newState = {
+            ...formValue,
+            [targetName]: targetValue,
+            enrollmentDate: '',
+            fuel: '',
+        };
+        setFormValue(newState);
+    } else if (targetName === 'enrollmentDate') {
+        newState = {
+            ...formValue,
+            [targetName]: targetValue,
+            fuel: '',
+        };
+        setFormValue(newState);
+    } else {
+        newState = {
+            ...formValue,
+            [targetName]: targetValue,
+        };
+        setFormValue(newState);
+    }
+};
