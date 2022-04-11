d43a6fe7bcaf367eaa0524b93f56ef1dd8536d89
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-11 12:42:40 +0200

    Abstracted render conditional function to another file

---
+++ src/components/SearchForm/renderDependingOnFetchCarsState.tsx
@@ -0,0 +1,32 @@
+import { ChangeEventHandler } from 'react';
+import { CarI } from '../../interfaces/cars.interfaces';
+import SelectBlock from '../Core/SelectBlock/SelectBlock';
+import Loading from '../Loading/Loading';
+import DisplayError from '../Error/Error';
+
+export const renderDependingOnFetchCarsState = (
+    loading: boolean,
+    cars: CarI[],
+    selectedCar: CarI,
+    handleSelect: ChangeEventHandler,
+    error: Error
+) => {
+    if (loading) {
+        return <Loading />;
+    }
+    if (cars.length) {
+        return (
+            <SelectBlock
+                label="Modelo"
+                value={selectedCar.model}
+                handleChange={handleSelect}
+                options={cars.map((car) => car.model)}
+                name="model"
+            />
+        );
+    }
+    if (!cars.length && !error) {
+        return <DisplayError message="No se encontró ningún modelo." />;
+    }
+    return <DisplayError message={error?.message as string} />;
+};
